062bfc68a8f12a3dc9062ee49dc82dc23ef59afa
elikemowusu <owusuderricke@gmail.com>
2022-02-16 14:39:59 -0800

    header file and updt

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,19 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+size_t height_l = 0;
+size_t height_r = 0;
 
-	if (!tree)
-		return (0);
+if (!tree)
+return (0);
 
-	if (tree->left)
-		height_l = 1 + binary_tree_height(tree->left);
+if (tree->left)
+height_l = 1 + binary_tree_height(tree->left);
 
-	if (tree->right)
-		height_r = 1 + binary_tree_height(tree->right);
+if (tree->right)
+height_r = 1 + binary_tree_height(tree->right);
 
-	if (height_l > height_r)
-		return (height_l);
-	return (height_r);
+if (height_l > height_r)
+return (height_l);
+return (height_r);
 }
