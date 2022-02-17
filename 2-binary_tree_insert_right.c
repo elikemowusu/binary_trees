f16872e272c88ecc3dd8d919d5f23df844a7063e
elikemowusu <owusuderricke@gmail.com>
2022-02-16 16:11:44 -0800

    updt of file1 -9

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,22 +9,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *right_node = NULL;
+	binary_tree_t *right_node = NULL;
 
-if (!parent)
-return (NULL);
+	if (!parent)
+		return (NULL);
 
-right_node = binary_tree_node(parent, value);
+	right_node = binary_tree_node(parent, value);
 
-if (!right_node)
-return (NULL);
+	if (!right_node)
+		return (NULL);
 
-if (parent->right)
-{
-right_node->right = parent->right;
-right_node->right->parent = right_node;
-}
-parent->right = right_node;
+	if (parent->right)
+	{
+		right_node->right = parent->right;
+		right_node->right->parent = right_node;
+	}
+	parent->right = right_node;
 
-return (right_node);
+	return (right_node);
 }
